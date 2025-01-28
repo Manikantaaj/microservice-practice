6d8dd97284b02bc2b0350ccde1ef6d2d7866578e
Varsha H C. <varsha.hc@excelindia.com>
2025-01-28 12:59:11 +0530

    entity-dto-class added

---
+++ spring-microservice-app/spring-microservice-core/src/main/java/com/excel/socket/core/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.excel.socket.core.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private String userId;
+    private  String name;
+    private String email;
+    private String password;
+}
---
+++ spring-microservice-app/spring-microservice-core/src/main/java/com/excel/socket/core/model/User.java
@@ -0,0 +1,21 @@
+package com.excel.socket.core.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String userId;
+    private  String name;
+    private String email;
+    private String password;
+
+}
