6ff9d6b8d7c4fa830d08f5a2f65561ab83cb123b
AkshayKumarBheemesh <akshaykumar.bheemesh@pearson.com>
2025-01-28 12:09:57 +0530

    microservice created

---
+++ spring-microservice-app/spring-microservice-composite/src/main/java/com/excel/socket/composite/App.java
@@ -0,0 +1,10 @@
+package com.excel.socket.composite;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
